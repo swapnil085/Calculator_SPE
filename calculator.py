452a629ccb31ecaad6d4d0b6a5aa106ca4fda00b
rishi12398 <rishi12398@gmail.com>
2020-04-29 18:18:29 +0530

    Basic_Calculator

---
+++ calculator.py
@@ -0,0 +1,21 @@
+class Claculator:
+    def add(self, x, y):
+        return x+y
+    def sub(self, x, y):
+        return x-y
+    def mul(self, x, y):
+        return x*y
+    def div(self, x, y):
+        try:
+            return x/float(y)
+        except:
+            return "Failed To Divide!"
+
+if __name__ == "__main__":
+    x = int(input("Enter 1st Value: "))
+    y = int(input("Enter 1st Value: "))
+    c= Claculator()
+    print(c.add(x,y))
+    print(c.sub(x,y))
+    print(c.mul(x,y))
+    print(c.div(x,y))
