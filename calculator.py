4c2bff8a4e82349d2455ce2cb20809d12a805ae7
rishi12398 <rishi12398@gmail.com>
2020-05-06 16:47:42 +0530

    ELK_stack_logs_added

--- calculator.py
+++ calculator.py
@@ -1,3 +1,5 @@
+import logging
+
 class Claculator:
     def add(self, x, y):
         return x+y
@@ -11,13 +13,34 @@ class Claculator:
         except:
             return "Failed To Divide!"
 
+
+
 if __name__ == "__main__":
     #x = int(input("Enter 1st Value: "))
     #y = int(input("Enter 1st Value: "))
     x = 6
     y = 9
-    c= Claculator()
-    print(c.add(x,y))
-    print(c.sub(x,y))
-    print(c.mul(x,y))
-    print(c.div(x,y))
+    c = Claculator()
+
+    add = c.add(x,y)
+    sub = c.sub(x,y)
+    mul = c.mul(x,y)
+    div = c.div(x,y)
+    print(add)
+    print(sub)
+    print(mul)
+    print(div)
+
+    logging.basicConfig(filename="logFile.txt",
+                    filemode='a',
+                    format='%(asctime)s %(levelname)s-%(message)s',
+                    datefmt='%Y-%m-%d %H:%M:%S', level=logging.DEBUG)
+    
+    logging.info("Inputs : %d and %d", x, y)
+    logging.info("Summation : %d", add)
+    logging.info("Difference : %d", sub)
+    logging.info("Multiplication : %d", mul)
+    logging.info("Division : %d", div)
+
+
+
